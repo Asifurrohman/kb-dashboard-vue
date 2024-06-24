64e80e7ff63d6c235ce5983a95395c81f2a8a7ea
Muhammad Asif <asifu@hoshizora.org>
2024-06-24 16:34:24 +0700

    Modify: submenu

--- src/views/adikbintangView/MyAdik.vue
+++ src/views/adikbintangView/MyAdik.vue
@@ -2,10 +2,22 @@
     <div class="w-full lg:w-4/5 absolute right-0 ">
         <main id="main" class=" mt-28 p-5">
             <!-- <h1 class="font-semibold text-xl">Adik Bintang Saya</h1> -->
-            <Submenu>
-                <a href="#" class="hover:text-hz-blue duration-150 px-2 py-3">Aktif</a>
-            </Submenu>
-
+            
+            <div>
+                <ul class="flex gap-4">
+                    <li class="border-b-4 border-b-transparent hover:border-dark-blue-1 duration-150 px-2 py-3">
+                        <a href="#" class="hover:text-dark-blue-1 font-semibold duration-150 px-2 py-3">Aktif</a>
+                    </li>
+                    <li class="border-b-4 border-b-transparent hover:border-dark-blue-1 duration-150 px-2 py-3">
+                        <a href="#" class="hover:text-dark-blue-1 font-semibold duration-150 px-2 py-3">Menunggu Persetujuan</a>
+                    </li>
+                    <li class="border-b-4 border-b-transparent hover:border-dark-blue-1 duration-150 px-2 py-3">
+                        <a href="#" class="hover:text-dark-blue-1 font-semibold duration-150 px-2 py-3">Selesai</a>
+                    </li>
+                </ul>
+                <hr class="h-1 bg-slate-400 -mt-1">
+            </div>
+            
             <div class="bg-hz-white p-2 rounded my-4">
                 <h2 class="text-lg font-bold">Ingin mentransfer donasi Anda?</h2>
                 <p>
@@ -15,11 +27,11 @@
                     Bayar Donasi
                 </button>
             </div>
-
+            
             <div class="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
                 <CardAdikBintang>
                     <template v-slot:card>
-                        <img src="../../../public/img/sakamoto.jpg" alt="">
+                        <img src="../../../public/img/sakamoto.jpg" alt="" class="h-40 w-full object-cover rounded-t">
                         <div class="p-2">
                             <h3 class="font-semibold mb-1 text-lg">Vladimir Monomakh</h3>
                             <div class="flex gap-1 mb-1 text-sm md:text-base">
@@ -47,7 +59,7 @@
                 
                 <CardAdikBintang>
                     <template v-slot:card>
-                        <img src="../../../public/img/sakamoto.jpg" alt="">
+                        <img src="../../../public/img/masha.jpg" alt="" class="h-40 w-full object-cover rounded-t">
                         <div class="p-2">
                             <h3 class="font-semibold mb-1 text-lg">Max Immelmann</h3>
                             <div class="flex gap-1 mb-1 text-sm md:text-base">
@@ -72,10 +84,10 @@
                         </div>
                     </template>
                 </CardAdikBintang>
-
+                
                 <CardAdikBintang>
                     <template v-slot:card>
-                        <img src="../../../public/img/sakamoto.jpg" alt="">
+                        <img src="../../../public/img/acipu.jpeg" alt="" class="h-40 w-full object-cover rounded-t">
                         <div class="p-2">
                             <h3 class="font-semibold mb-1 text-lg">Sutan Sjahrir</h3>
                             <div class="flex gap-1 mb-1 text-sm md:text-base">
@@ -97,8 +109,10 @@
                                     Saya mempunyai cita-cita menjadi guru karena guru adalah sosok yang mulia dan pi...
                                 </p>
                             </div>
+import Adikbintang from '../adikbintang/Index.vue';
                         </div>
                     </template>
+                    import Adikbintang from '../adikbintang/Index.vue';
                 </CardAdikBintang>
             </div>
         </main>
@@ -107,8 +121,8 @@
 
 
 <script setup>
-    import CardAdikBintang from '../../components/CardAdikBintang.vue'
-    import Submenu from '../../components/Submenu.vue'
+import CardAdikBintang from '../../components/CardAdikBintang.vue'
+import Submenu from '../../components/Submenu.vue'
 </script>
 <script>
 
